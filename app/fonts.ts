c6f12d3586ddeda7ae86e00ee93020b26d635e34
akramfirmansyah <akram.firman@gmail.com>
2024-01-29 13:32:30 +0800

    initial setup

---
+++ app/fonts.ts
@@ -0,0 +1,11 @@
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
+
+export const fonts = {
+  poppins,
+};
